076067e3383e9f03c615fb44182ceffde01114a9
Tanishk405 <your-email@example.com>
2026-04-03 11:14:04 +0530

    small change again

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function middleware(req: NextRequest){
+export async function proxy(req: NextRequest){
     const token = await getToken({req,
         secret: process.env.NEXT_AUTH_SECRET
     })
